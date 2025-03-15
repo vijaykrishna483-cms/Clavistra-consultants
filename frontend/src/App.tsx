ef7a59bce13509f334e2012ab926397dc183177a
vijaykrishna483 <vijaykrshns@gmail.com>
2025-03-15 15:56:59 +0530

    DFDF

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
-import Performance from "./Components/Expertise/Pages/Socialmedia";
+import Socialmedia from "./Components/Expertise/Pages/Socialmedia";
 import BrandAnalysis from "./Components/Expertise/Pages/BrandAnalysis";
 import BrandStrategy from "./Components/Expertise/Pages/Influencer";
 import Website from "./Components/Expertise/Pages/Financial";
@@ -14,7 +14,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/performancemarketing" element={<Performance />} />
+        <Route path="/performancemarketing" element={<Socialmedia />} />
         <Route path="/influencer" element={<BrandStrategy />} />
         <Route path="/brandanalysis" element={<BrandAnalysis />} />
         <Route path="/financial" element={<Website />} />
